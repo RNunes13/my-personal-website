0b45bd72fa64bc2b951ec086726e4c72c0f817fb
Rodrigo Nunes <nunesrodrigo13@outlook.com>
2020-08-18 12:08:57 -0300

    Project started! :rocket:

---
+++ src/app/App.tsx
@@ -0,0 +1,13 @@
+
+import React from 'react';
+import './App.scss';
+
+function App() {
+  return (
+    <main className="my-app">
+      <h1>Rodrigo Nunes | Web Developer</h1>
+    </main>
+  );
+}
+
+export default App;
